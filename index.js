bf6eea3cefa7ffa6b8c02f0be6fa510eb1c9d3ee
Anıl Güleroğlu <anil_guleroglu@hotmail.com>
2016-12-31 11:36:54 +0300

    articles,columns and search added.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Deneme")
+
+var hurriyet = require("./hurriyet.js")
+
+
+var api = hurriyet("327c396d84c9491982f32e7c3625f908");
+
+// api.articles(function(res){
+
+//     console.log(res.length);
+
+// });
+
+// api.article('40322757',function(res){
+
+//     console.log(res);
+
+// });
+
+// api.columns(function(res){
+
+//     console.log(res);
+
+// });
+
+// api.column("40321556",function(res){
+
+//     console.log(res);
+
+// })
+
+api.search('hackathon',{top:1},function(res){
+    console.log(res);
+})
