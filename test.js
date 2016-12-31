e0e8a2b76e9cd8f0cc6d1ede0fe39dee3a52274c
Anıl Güleroğlu <anil_guleroglu@hotmail.com>
2016-12-31 12:19:14 +0300

    edit README.md

--- test.js
+++ test.js
@@ -4,29 +4,29 @@ var hurriyet = require("./index.js")
 
 var api = hurriyet("327c396d84c9491982f32e7c3625f908");
 
-// api.articles(function(res){
+api.articles(function(res){
 
-//     console.log(res.length);
+    console.log(res.length);
 
-// });
+});
 
-// api.article('40322757',function(res){
+api.article('40322757',function(res){
 
-//     console.log(res);
+    console.log(res);
 
-// });
+});
 
-// api.columns(function(res){
+api.columns(function(res){
 
-//     console.log(res);
+    console.log(res);
 
-// });
+});
 
-// api.column("40321556",function(res){
+api.column("40321556",function(res){
 
-//     console.log(res);
+    console.log(res);
 
-// })
+})
 
 api.search('hackathon',{top:1},function(res){
     console.log(res);
